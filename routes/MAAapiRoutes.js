6ba6708ac6c1eb39d91b035c7e1298efed6bf325
LindoK <khavin.lindo@gmail.com>
2019-09-17 20:11:33 -0400

    Edited route files

--- routes/MAAapiRoutes.js
+++ routes/MAAapiRoutes.js
@@ -1,20 +1,21 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
+
 
 // Dependencies
 // =============================================================
-var Grocery = require("../models/Grocery.js");
-var Task = require("../models/Task.js");
-var Notes = require("../models/Notes.js");
+var Grocery = require("../models/grocery.js");
+var Task = require("../models/task.js");
+var Notes = require("../models/notes.js");
 
-// Routes
-// =============================================================
-
-// Grocery List -- ID, Item, Catagory
-//    catagories -- 1) produce 2) meats, 3) dairy, 4) dry goods, 5) non-grocery
-
+//=============================================================//
+//Routes
+//=============================================================//
 module.exports = function(app) {
+  
+
+  /** Grocery List -- ID, Item, Catagory=======================================
+    catagories -- 1) produce 2) meats, 3) dairy, 4) dry goods, 5) non-grocery*/
+  //===========================================================================
+  
   // Get all groceries
   app.get("/api/all", function(req, res) {
     Grocery.findAll({}).then(function(results) {
@@ -22,65 +23,17 @@ module.exports = function(app) {
     });
   });
 
-  // Get a specific catagory
+  // Get a grocery by specific catagory
   app.get("/api/:catagory", function(req, res) {
     Grocery.findAll({
       where: {
-        : req.params.catagory
+       category : req.params.catagory
       }
     }).then(function(results) {
       res.json(results);
     });
   });
 
-  //====================================================
-  // Task List -- ID, Task, Priority
-  //    urgency -- 1) Top Priority, 2) Done Today, 3) This Week
-
-  // Get all Tasks 
-  module.exports = function(app) {
-    // Get all groceries
-    app.get("/api/all", function(req, res) {
-      Task.findAll({}).then(function(results) {
-        res.json(results);
-      });
-    });
-  
-    // Get a specific catagory
-    app.get("/api/:urgency", function(req, res) {
-      Task.findAll({
-        where: {
-          : req.params.urgency
-        }
-      }).then(function(results) {
-        res.json(results);
-      });
-    });
-
-  //=====================================================
-  //Notes -- ID, Note, Author
-
-  // Get all Notes
-  module.exports = function(app) {
-    // Get all Notes
-    app.get("/api/all", function(req, res) {
-      Notes.findAll({}).then(function(results) {
-        res.json(results);
-      });
-    });
-  
-    // Get a specific Author
-    app.get("/api/:author", function(req, res) {
-      Task.findAll({
-        where: {
-          : req.params.author
-        }
-      }).then(function(results) {
-        res.json(results);
-      });
-    });
-
-  //=====================================================
 
   // Add a Grocery Item
   app.post("/api/new", function(req, res) {
@@ -94,7 +47,7 @@ module.exports = function(app) {
     });
   });
 
-  // Delete a Grocery Item
+// Delete a Grocery Item
   app.delete("/api/grocery/:id", function(req, res) {
     console.log("Grocery ID:");
     console.log(req.params.id);
@@ -106,9 +59,36 @@ module.exports = function(app) {
       res.end();
     });
   });
-};
+  
+  
+  
+  
+  
+  //====================================================
+  // Task List -- ID, Task, Priority
+  //    urgency -- 1) Top Priority, 2) Done Today, 3) This Week
+  //=========================================================
+    
+  
+  //Get all tasks
+  app.get("/api/all", function(req, res) {
+      Task.findAll({}).then(function(results) {
+        res.json(results);
+      });
+    });
+  
+  // Get a task by specific catagory
+    app.get("/api/:urgency", function(req, res) {
+      Task.findAll({
+        where: {
+          urgency : req.params.urgency
+        }
+      }).then(function(results) {
+        res.json(results);
+      });
+    });
 
-//========================================================
+  
   // Add a Task
   app.post("/api/new", function(req, res) {
     console.log("Task Data:");
@@ -120,7 +100,7 @@ module.exports = function(app) {
       res.json(results);
     });
   });
-
+  
   // Delete a Task
   app.delete("/api/task/:id", function(req, res) {
     console.log("Task ID:");
@@ -133,9 +113,34 @@ module.exports = function(app) {
       res.end();
     });
   });
-};
+  
+  
 
-//========================================================
+
+  //=====================================================
+  //Notes -- ID, Note, Author
+  //=====================================================  
+  
+  // Get all Notes
+    app.get("/api/all", function(req, res) {
+      Notes.findAll({}).then(function(results) {
+        res.json(results);
+      });
+    });
+  
+    
+    // Get a note by specific Author
+    app.get("/api/:author", function(req, res) {
+      Task.findAll({
+        where: {
+          author : req.params.author
+        }
+      }).then(function(results) {
+        res.json(results);
+      });
+    });
+
+  
   // Add a Note
   app.post("/api/new", function(req, res) {
     console.log("Note Data:");
@@ -147,6 +152,7 @@ module.exports = function(app) {
       res.json(results);
     });
   });
+  
 
   // Delete a Note
   app.delete("/api/note/:id", function(req, res) {
