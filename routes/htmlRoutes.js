42fb22ebcf1ba54f1a98ce77ddd754937ac5a632
Ryan Weingart <rmweingart5@gmail.com>
2019-09-18 21:37:47 -0400

    added html through handlebars for task and grocery

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
+      res.render("grocery", {
         msg: "Day Planner",
         examples: dbExamples
       });
@@ -24,4 +24,5 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.render("404");
   });
+
 };
